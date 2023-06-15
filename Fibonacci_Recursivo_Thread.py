33cb20b98c702cd11a40db12a0f76b0d30e2c4b7
Luiz Gustavo da Costa Gonçalves <luizg.dev@gmail.com>
2023-06-15 14:38:38 -0300

    Adicionando comentários.

--- Fibonacci_Recursivo_Thread.py
+++ Fibonacci_Recursivo_Thread.py
@@ -5,18 +5,42 @@ import time
 # Classe para calcular Fibonacci em uma thread separada
 class FibonacciThread(threading.Thread):
     def __init__(self, n):
+        """
+        Inicializa uma nova instância de FibonacciThread.
+
+        Args:
+            n (int): Número do termo desejado na sequência.
+        """
         threading.Thread.__init__(self)
         self.n = n
         self.result = None
 
     def run(self):
+        """
+        Método executado pela thread. Calcula o termo da sequência de Fibonacci.
+        """
         self.result = fibonacci_recursivo(self.n)
 
     def get_result(self):
+        """
+        Obtém o resultado calculado pela thread.
+
+        Returns:
+            int: Resultado do cálculo.
+        """
         return self.result
 
 
 def fibonacci_recursivo(n):
+    """
+    Função recursiva para calcular o n-ésimo termo da sequência de Fibonacci.
+
+    Args:
+        n (int): Número do termo desejado na sequência.
+
+    Returns:
+        int: O n-ésimo termo da sequência de Fibonacci.
+    """
     if n <= 1:
         return n
     else:
@@ -24,6 +48,15 @@ def fibonacci_recursivo(n):
 
 
 def fib_rec_thread(n):
+    """
+    Calcula a sequência de Fibonacci usando threads.
+
+    Args:
+        n (int): Número de termos da sequência desejados.
+
+    Returns:
+        list: Lista contendo a sequência de Fibonacci calculada.
+    """
     if n <= 1:
         return n
     else:
@@ -42,9 +75,10 @@ def fib_rec_thread(n):
 
 
 def main():
+    # Define o número de termos da sequência de Fibonacci que desejamos calcular
     n = 40
 
-    # Uso fibonacci recursivo:
+    # Uso fibonacci recursivo com threads:
     inicio = time.time()
     sequencia = [fib_rec_thread(i) for i in range(1, n + 1)]
     final = time.time() - inicio
@@ -53,4 +87,5 @@ def main():
 
 
 if __name__ == '__main__':
+    # Chama a função principal do programa
     main()
